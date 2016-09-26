ab7ba89b696efe2416b0a02788dd1907ede20d6d
danieldkato <daniel.d.kato@gmail.com>
2016-09-26 16:25:21 -0400

    Make Coor2txt create a directory for text files containing ROI coordinates

--- Coor2txt.m
+++ Coor2txt.m
@@ -1,13 +1,19 @@
 struct = load('Coor.mat');
 Coor = struct.Coor;
+outputDirectoryName = 'Coor2txt';
 
-for i = 1:length(Coor)
-%for i = 1:1
-    id = fopen(strcat(['ROI_', num2str(i), '_coords.txt']), 'wt');
-    currentROI = Coor{i};
-    for j = 1:length(currentROI)
-        fprintf(id, [num2str(currentROI(1,j)), ' ']);
-        fprintf(id, [num2str(currentROI(2,j)), '\n']);        
+A = exist(outputDirectoryName, 'dir');
+
+if A ~= 7 %if there does not already exist a directory called 'Coor2txt'
+    mkdir(outputDirectoryName);
+    cd(outputDirectoryName);
+    for i = 1:length(Coor)
+        id = fopen(strcat(['ROI_', num2str(i), '_coords.txt']), 'wt');
+        currentROI = Coor{i};
+        for j = 1:length(currentROI)
+            fprintf(id, [num2str(currentROI(1,j)), ' ']);
+            fprintf(id, [num2str(currentROI(2,j)), '\n']);        
+        end
+        fclose(id);
     end
-    fclose(id);
 end

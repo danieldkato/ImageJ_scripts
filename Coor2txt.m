ad4a534fd1852b7f1d570c1c8c78f21c6833a272
danieldkato <daniel.d.kato@gmail.com>
2016-09-26 18:17:15 -0400

    Write documentation for Coor2txt.m

--- Coor2txt.m
+++ Coor2txt.m
@@ -1,3 +1,39 @@
+%Last updated DDK 2016-09-26
+
+% OVERVIEW:
+% This script is used for exporting automatically-drawn ROIs from Eftykios
+% Pnevmatikakis' ca_source_extraction software to a .txt format that can
+% then be read into Fiji for manual refinement. 
+
+% REQUIREMENTS:
+% This script is meant to be run on the output of Eftykios Pnevmatikakis'
+% ca_source_extraction software, available at:
+
+% https://github.com/epnev/ca_source_extraction/blob/master/CSHL16/demo_somatic.m(commit ff3c6b6) 
+
+
+% INSTRUCTIONS:
+% Before running this script, run ca_source_extraction on the multi-page
+% TIFF to be segmented. Save the output cell array Coor as a .mat file. 
+
+% Run this script from the directory in which Coor.mat is saved. For each
+% ROI, the script will create a two-column .txt file, where each row is a
+% point defining the contours of the ROI, and the first and second columns
+% represent X- and Y- coordinates of each point, respectively. The script
+% will then save these .txt files into a directory called Coor2txt. If the
+% directory already exists, the script will ask the user whether to
+% overwrite the existing directory, create a new directory, or cancel the
+% operation. 
+
+% These .txt can then be loaded one at a time into Fiji using File>Import>XY
+% Coordinates... . Alternatively, they can be imported as a batch using the
+% Batch Import ROIs macro, available at: 
+
+% https://github.com/danieldkato/cse2Fiji/blob/master/importROIs.txt
+
+
+
+%%
 struct = load('Coor.mat');
 Coor = struct.Coor;
 outputBaseName = 'Coor2txt';
@@ -8,12 +44,14 @@ while exportComplete == 0;
     if A == 7 %If the directory Coor2txt already exists, ask the user for appropriate action
         button = questdlg('The directory Coor2txt already exists. Select whether to overwrite existing directory, create a new directory, or cancel the operation.', 'Confirm folder replace', 'Overwrite', 'Create new directory', 'Cancel', 'Cancel');
         switch button
+            
             case 'Overwrite'
                 %If the user indicates that it's ok to overwrite the
                 %existing directory, then just name the output directory
                 %the base name.
                 rmdir(outputBaseName, 's');
                 outputDirectory = outputBaseName;
+            
             case 'Create new directory'
                 %If the user says to create a new directory, then add a
                 %numeric suffix to the output directory base name. The
@@ -34,6 +72,7 @@ while exportComplete == 0;
                 end
                 newSuffix = maxSuffix + 1;
                 outputDirectory = strcat(outputBaseName, '_', num2str(newSuffix));
+            
             case 'Cancel'
                 %If the user says to cancel the operation, then mark the
                 %export as complete and break the the while loop so that
